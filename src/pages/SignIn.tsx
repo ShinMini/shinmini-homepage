eae3747b4fa2ad421351c7f29d00ec249e668d7b
Sotatek-shinmini <shinmini@sotatek.com>
2023-06-02 19:30:50 +0900

    chore: need to merge to App

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,21 +1,14 @@
 import GoogleButton from 'react-google-button';
-
-import { auth } from '../lib/firebase/firebase';
-import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth/cordova';
+import { signIn } from '../utils/signIn';
 
 const style = {
   wrapper: `flex justify-center`,
 };
 
-const googleSignIn = () => {
-  const Provider = new GoogleAuthProvider();
-  signInWithRedirect(auth, Provider);
-};
-
 const SignIn = () => {
   return (
     <div className={style.wrapper}>
-      <GoogleButton onClick={googleSignIn} />
+      <GoogleButton onClick={signIn} />
     </div>
   );
 };
