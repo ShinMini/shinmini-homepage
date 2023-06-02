77a422d7d19bc8f40fadddbcdb64d7cf4e056ef2
Sotatek-shinmini <shinmini@sotatek.com>
2023-06-02 23:09:50 +0900

    docs: add git action & eslint automation

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,8 @@
 // .eslintrc.js
-export default {
+module.exports = {
+  ignorePatterns: ['node_modules/', 'build/', 'public/'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['react-app', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  exclude: ['/node_modules/*', '/public/*'],
-  rules: {
-    // 여기에 추가 규칙을 정의할 수 있습니다.
-  },
+  rules: {},
 };
