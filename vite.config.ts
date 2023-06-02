f324775e849df9a7d37f96cea30b800541971c15
Sotatek-shinmini <shinmini@sotatek.com>
2023-06-02 19:28:32 +0900

    docs: default set up files

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    sourcemap: true,
+    outDir: 'public',
+  },
 });
