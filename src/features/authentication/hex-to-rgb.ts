3f0bda82e1a3c918408fdf25416671922d4126ab
ShinMini <77220824+ShinMini@users.noreply.github.com>
2023-06-18 16:54:01 +0900

    fix(bug): hex to rgba error

--- src/features/authentication/hex-to-rgb.ts
+++ src/features/authentication/hex-to-rgb.ts
@@ -1,8 +1,13 @@
 export const hexToRGBA = (hex: string, opacity = 0.2) => {
-  const hexCode = hex.replace('#', '');
-  const r = parseInt(hexCode.substring(0, 2), 16);
-  const g = parseInt(hexCode.substring(2, 4), 16);
-  const b = parseInt(hexCode.substring(4, 6), 16);
+  try {
+    const hexCode = hex.replace('#', '');
+    const r = parseInt(hexCode.substring(0, 2), 16);
+    const g = parseInt(hexCode.substring(2, 4), 16);
+    const b = parseInt(hexCode.substring(4, 6), 16);
 
-  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+  } catch (e) {
+    console.error(e);
+    return hex;
+  }
 };
