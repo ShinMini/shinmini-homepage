9a238554c2100527008708fabd5099d91747509c
Sotatek-shinmini <shinmini@sotatek.com>
2023-06-24 16:31:00 +0900

    feat(photo): optimizing pictures

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,10 +3,18 @@ import FavIcon from './icons/logo.svg';
 
 // IMAGES
 import GreetingBackground from './images/greeting-background.png';
-import ProfileImage from './images/profile-image.png';
+import ProfileImageLarge from './images/home/profile-image:626X626.png';
+import ProfileImageMedium from './images/home/profile-image:400X400.png';
+import ProfileImageSmall from './images/home/profile-image:260X260.png';
 
 const Icons = {
   FavIcon,
+};
+
+const ProfileImage = {
+  Large: ProfileImageLarge,
+  Medium: ProfileImageMedium,
+  Small: ProfileImageSmall,
 };
 
 const Images = {
