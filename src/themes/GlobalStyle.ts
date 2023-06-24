83c50d1a86fd3d8a2012f247744cdcbb59789476
Sotatek-shinmini <shinmini@sotatek.com>
2023-06-24 17:05:30 +0900

    feat(dep): add lib - mui, emailjs

--- src/themes/GlobalStyle.ts
+++ src/themes/GlobalStyle.ts
@@ -1,5 +1,9 @@
-import { createGlobalStyle } from 'styled-components';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
+import { createGlobalStyle } from 'styled-components';
 import { Poppins } from '@src/assets/fonts';
 
 const GlobalStyle = createGlobalStyle`
