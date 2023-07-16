c669f6cbda2eeeb3ff6745f70aee4e2f91c671de
ShinMini <gusals121234@gmail.com>
2023-07-16 17:39:33 +0900

    파이어베이스발 내가 너 기억한다

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,16 @@
 import { defineConfig, loadEnv } from 'vite';
-import EnvironmentPlugin from 'vite-plugin-environment';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// import electron from 'vite-plugin-electron';
-// import renderer from 'vite-plugin-electron-renderer';
-
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd());
   console.log('env: ', env);
 
   return {
     define: {
       'process.env': env,
     },
-    plugins: [
-      react(),
-      tsconfigPaths(),
-      EnvironmentPlugin('all'),
-      // electron([
-      //   {
-      //     // Main-Process entry file of the Electron App.
-      //     entry: 'electron/main.ts',
-      //   },
-      //   {
-      //     entry: 'electron/preload.ts',
-      //     onstart(options) {
-      //       // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-      //       // instead of restarting the entire Electron App.
-      //       options.reload();
-      //     },
-      //   },
-      // ]),
-      // renderer(),
-    ],
+    plugins: [react(), tsconfigPaths()],
     build: {
       sourcemap: true,
       outDir: 'public',
